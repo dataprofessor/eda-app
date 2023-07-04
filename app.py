b88a9a07e67d301b0196b4c59f17fcc95f8cad42
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-04 17:27:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-from pandas_profiling import ProfileReport
+#from pandas_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
 # Web App Title
